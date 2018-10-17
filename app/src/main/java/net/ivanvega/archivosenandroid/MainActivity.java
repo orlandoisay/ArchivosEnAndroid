ede932215a53e1cdfe634bdec438ebe37b557450
Orlando Isay <orlandoisay@gmail.com>
2018-10-16 20:35:23 -0500

    Se agrego verificacion de permisos en tiempo de ejecucion.

--- app/src/main/java/net/ivanvega/archivosenandroid/MainActivity.java
+++ app/src/main/java/net/ivanvega/archivosenandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package net.ivanvega.archivosenandroid;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,20 +27,58 @@ public class MainActivity extends AppCompatActivity {
     EditText txt ;
     RadioGroup optg ;
     RadioButton optSelecccionado;
+    final int WRITE_EXTERNAL_REQ_CODE = 777;
+    boolean allowedToWriteToExternal = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        revisarPermisos();
+
         txt = (EditText)findViewById(R.id.txt);
         optg = (RadioGroup)findViewById(R.id.optgAlmacenamiento);
         Log.d("CICLOVIDA", "onCreate");
     }
 
+    public void revisarPermisos() {
+        int permissionCheck =
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_REQ_CODE);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+        @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case WRITE_EXTERNAL_REQ_CODE: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    allowedToWriteToExternal = true;
+                } else {
+                    allowedToWriteToExternal = false;
+                }
+            }
+        }
+
+    }
+
     public void btnGuardar_click(View v) {
         if(	optg.getCheckedRadioButtonId() == R.id.optExterna) {
-            saveExternal();
+            if (allowedToWriteToExternal) {
+                saveExternal();
+            } else {
+                Toast.makeText(this, "No hay permisos suficientes", Toast.LENGTH_LONG).show();
+            }
         } else {
             saveInternal();
         }
